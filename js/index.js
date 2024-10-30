596c40191b1ebc2f165ed50ccbcb1e43218dd3f5
MoiPad <moipad85@gmail.com>
2024-10-29 20:36:27 -0600

    finalizacin del ejercicio

--- js/index.js
+++ js/index.js
@@ -1,27 +1,44 @@
 // codigo js
 
-
-fetch("https://api.escuelajs.co/api/v1/users")
+fetch("https://jsonplaceholder.typicode.com/users")
   .then((response) => response.json())
   .then((data) => {
-    let cards = ''
+    console.log(data);
+
+    let card = '<div class="row row-cols-1 rows-cols-md3 g-4">';
 
     for (let user of data) {
-      cards += `
-        <div class='col-4'>
-          <div class='card' style='width: 18rem;'>
-            <img src='${user.avatar}' class='card-image-top' alt='image avatar'>
-            <div  class ='card-body'>
-                <h5 class='card-title'>${user.name}</h5>
-                <p class ='card-text'>${user.email}</p>
-                <p class='card-text'>${user.password}</p>
-              <a href='#' class='btn btn-primary'>Mas información</a>
+      card += `
+                <div class="card">
+            <img
+              src="https://www.cavsi.com/espanol/wp-content/uploads/informatica/usuario-de-computadoras.jpg"
+              alt="test image"
+              class="card-img-top"
+            />
+            <div class="card-body">
+<h4 class="card-title">${user.name}</h5>
+
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">
+                    <strong>Nombre de usuairo: </strong>${user.username}
+                </li>
+                <li class="list-group-item">
+                    <strong>Correo eletrnico: </strong>${user.email}
+                </li>
+                <li class="list-group-item">
+                    <strong>Calle: </strong> ${user.address.street}
+                </li>
+                    <li class="list-group-item"><strong>Ciudad: </strong>${user.address.city}</li>
+                <li class="list-group-item">
+                    <strong>Telefono: </strong>${user.phone}
+                </li>
+                <li class="list-group-item">
+                    <strong>Compania: </strong>${user.company.name}
+                </li>
+              </ul>
             </div>
-          </div>
-        </div>
-
-        `
-      document.getElementById('api-data').innerHTML = cards
+  `;
     }
-
-  })
+    card += "</div>";
+    document.getElementById("card-list").innerHTML = card;
+  });
